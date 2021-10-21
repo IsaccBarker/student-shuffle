b8f3dc2f0c15af78c12e43d8cd33ade300c62389
IsaccBarker <plutoisaplanet4324@gmail.com>
2021-10-21 14:03:43 -0600

    added default button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,38 @@
+function go(src) {
+    var names = [];
+    var data = Papa.parse(src);
+
+    console.log(data);
+
+    for (row of data.data) {
+        names.push(row[1]);
+    }
+
+    names.sort(() => Math.random() - 0.5);
+    names.shift();
+    const half = Math.ceil(names.length / 2);
+
+    const firstHalf = names.splice(0, half)
+    const secondHalf = names.splice(-half)
+
+    document.getElementById("classOneHeader").style.display = "block";
+    document.getElementById("classTwoHeader").style.display = "block";
+
+    document.getElementById("output1").innerHTML = '<p>' + firstHalf.join('<br>') + '</p>';
+    document.getElementById("output2").innerHTML = '<p>' + secondHalf.join('<br>') + '</p>';
+}
+
+function handleDefault() {
+    fetch('/juniorclass.csv')
+        .then(res => res.json())
+        .then((out) => {
+            go(out);
+        })
+        .catch(err => {
+            throw err
+        });
+}
+
 function handleFileSelect(evt) {
     console.log("User uploaded file");
     var files = evt.target.files; // FileList object
@@ -9,26 +44,7 @@ function handleFileSelect(evt) {
 
     // Closure to capture the file information.
     reader.onload = (function(theFile) {
-        return function(e) {
-            var names = [];
-            var data = Papa.parse(e.target.result);
-
-            console.log(data);
-
-            for (row of data.data) {
-                names.push(row[1]);
-            }
-
-            names.sort(() => Math.random() - 0.5);
-            names.shift();
-            const half = Math.ceil(names.length / 2);
-
-            const firstHalf = names.splice(0, half)
-            const secondHalf = names.splice(-half)
-
-            document.getElementById("output1").innerHTML = firstHalf.join('<br>');
-            document.getElementById("output2").innerHTML = secondHalf.join('<br>')
-        };
+        return function(e) { go(e.target.result) };
     })(f);
 
     // Read in the image file as a data URL.
